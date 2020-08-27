d68f977d575b997de8e04e125d8019e85140a89c
nishigothi <62197853+nishi812@users.noreply.github.com>
2020-08-27 18:48:40 +0530

    Hello world through react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ShoppingList extends Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+class App {
+  render(){
+    return(
+      <h1>hello</h1>
+    );
+  }
 }
 
-export default App;
+// Example usage: <ShoppingList name="Mark" />
+
+export default ShoppingList, default App;
